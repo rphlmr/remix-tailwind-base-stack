beda6daecbfbbfbb5136acf4c92867f880e5f537
rphlmr <raphael@uaerom.com>
2022-09-25 18:10:34 +0200

    init
    clean
    
    rename

---
+++ app/routes/index.tsx
@@ -0,0 +1,16 @@
+export default function Example() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center space-y-12">
+      <img
+        src="https://user-images.githubusercontent.com/20722140/192152869-ea60156f-1452-4f86-a1eb-8a9636884ba9.svg"
+        alt="Remix love Tailwind"
+        className="max-w-xs"
+      />
+
+      <h1 className="text-4xl font-bold tracking-tight text-neutral-50 sm:text-5xl md:text-6xl">
+        <span className="block xl:inline">Stay hungry,</span>{" "}
+        <span className="block text-sky-400 xl:inline">stay foolish</span>
+      </h1>
+    </div>
+  );
+}
---
+++ remix.init/index.js
@@ -0,0 +1,64 @@
+const { execSync } = require("child_process");
+const crypto = require("crypto");
+const fs = require("fs/promises");
+const path = require("path");
+
+const sort = require("sort-package-json");
+
+function getRandomString(length) {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+async function main({ rootDirectory, isTypeScript }) {
+  console.log(`🚀  Making something cool with this template ...`);
+
+  if (!isTypeScript) {
+    throw new Error("😌  Sorry, this template only supports TypeScript");
+  }
+
+  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
+  const ENV_PATH = path.join(rootDirectory, ".env");
+  const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json");
+
+  const DIR_NAME = path.basename(rootDirectory);
+  const SUFFIX = getRandomString(2);
+  const APP_NAME = (DIR_NAME + "-" + SUFFIX)
+    // get rid of anything that's not allowed in an app name
+    .replace(/[^a-zA-Z0-9-_]/g, "-");
+
+  const [env, packageJson] = await Promise.all([
+    fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
+    fs.readFile(PACKAGE_JSON_PATH, "utf-8").then((s) => JSON.parse(s)),
+  ]);
+
+  const newEnv = env.replace(
+    /^SESSION_SECRET=.*$/m,
+    `SESSION_SECRET="${getRandomString(16)}"`
+  );
+
+  const newPackageJson =
+    JSON.stringify(sort({ ...packageJson, name: APP_NAME }), null, 2) + "\n";
+
+  await Promise.all([
+    fs.writeFile(ENV_PATH, newEnv),
+    fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
+    fs.copyFile(
+      path.join(rootDirectory, "remix.init", "gitignore"),
+      path.join(rootDirectory, ".gitignore")
+    ),
+  ]);
+
+  execSync("npm run format -- --loglevel warn", {
+    stdio: "inherit",
+    cwd: rootDirectory,
+  });
+
+  console.log(
+    `Setup is complete. You're now ready to rock and roll 🤘
+
+Start development with \`npm run dev\`
+`.trim()
+  );
+}
+
+module.exports = main;
